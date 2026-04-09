4de5196712c0d36436675a10b6cbb7301621c8c0
santiherrera-0 <142118583+SantiHerrera-0@users.noreply.github.com>
2026-04-09 20:17:52 -0300

    Agregar efecto de dibujo
    -Se implemento delegacion de eventos con el elemento contenedor
    para hacer el rastro del mouse en la cuadricula.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,21 @@
-let contenido = document.querySelector(".contenedor");
-
-let elemento = [];
+let contenedor = document.querySelector(".contenedor");
+let elemento=[];
 
 for(let i=0; i<256; i++){
-    elemento[i] = document.createElement("div");
-    elemento[i].style.border = "solid grey";
-    elemento[i].style.height = "50px";
-    elemento[i].style.width = "50px";
-    contenido.appendChild(elemento[i]);
+    let nombreDeClase = i;
+    elemento = document.createElement("div");
+    elemento.style.border = "solid grey";
+    elemento.style.height = "50px";
+    elemento.style.width = "50px";
+    elemento.classList.add(nombreDeClase);
+    contenedor.appendChild(elemento);
 }
+
+contenedor.addEventListener("mouseover",(evento) => {
+    let claseCuadroActual = evento.target.className;
+    console.log(claseCuadroActual);
+    let cuadroActual = evento.target;
+    if(claseCuadroActual !== "contenedor"){
+        cuadroActual.style.backgroundColor="red";
+    }
+})
