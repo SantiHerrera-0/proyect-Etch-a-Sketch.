0ba387bfce06d7aa582c00e88a1f8ecb0e8f6ed4
santiherrera-0 <142118583+SantiHerrera-0@users.noreply.github.com>
2026-04-15 13:19:36 -0300

    Agregar formula para area de cuadrado y forma de grilla
    -Se agrego la formula para calcular el area de cada cuadrado de
    la grilla y se implemento en la creacion de la grilla personalizada.

--- javascript.js
+++ javascript.js
@@ -1,21 +1,45 @@
 let contenedor = document.querySelector(".contenedor");
-let elemento=[];
+let boton = document.querySelector(".boton")
+let cantidadActual = 0;
 
-for(let i=0; i<256; i++){
-    let nombreDeClase = i;
-    elemento = document.createElement("div");
-    elemento.style.border = "solid grey";
-    elemento.style.height = "50px";
-    elemento.style.width = "50px";
-    elemento.classList.add(nombreDeClase);
-    contenedor.appendChild(elemento);
+
+function crearGrilla(cantidad=50){
+/*Formula para el lado de un cuadro.*/
+    let dimensionCuadro = `${Math.sqrt((960**2)/cantidad**2)-6}px`;
+    for(let i=0; i<cantidad**2; i++){
+        let nombreDeClase = i;
+        let elemento = document.createElement("div");
+        elemento.style.border = "solid grey";
+        elemento.style.height = dimensionCuadro;
+        elemento.style.width =  dimensionCuadro;
+        elemento.classList.add(`cuadro${nombreDeClase}`);
+        contenedor.appendChild(elemento);
+        }
+    cantidadActual = cantidad;
+}
+
+
+function eliminarGrilla(){
+    let nodo = contenedor.childNodes;
+    console.log(nodo);
+    for(let i=cantidadActual**2; i>=0; i--){
+        contenedor.removeChild(nodo[i]);
+    }
+    console.log(nodo);
 }
 
 contenedor.addEventListener("mouseover",(evento) => {
     let claseCuadroActual = evento.target.className;
-    console.log(claseCuadroActual);
     let cuadroActual = evento.target;
     if(claseCuadroActual !== "contenedor"){
         cuadroActual.style.backgroundColor="red";
     }
 })
+
+boton.addEventListener("click",()=>{
+        eliminarGrilla();
+        let cantidad = +prompt("Ingresar cantidad de cuadros", 1);
+        crearGrilla(cantidad);
+})
+
+crearGrilla();
